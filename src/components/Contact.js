6f40f4f48d08863358ed589b031260be41d3b9a1
jrb3620 <jonathanbuckner@me.com>
2024-08-08 16:45:51 -0500

    bugfixing contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,17 +1,67 @@
 import React from "react";
 
 export default function Contact() {
+    function handleSubmit(event) {
+        event.preventDefault(); // Prevent the default form submission
+        const form = event.target;
+
+        const formData = new FormData(form);
+
+        // Use fetch to submit the form data to Netlify
+        fetch("/", {
+            method: "POST",
+            headers: { "Content-Type": "application/x-www-form-urlencoded" },
+            body: new URLSearchParams(formData).toString(),
+        })
+            .then(() => alert("Message sent!"))
+            .catch((error) => alert(error));
+    }
+
     return (
         <section id="contact" className="relative">
             <div className="container px-5 py-10 mx-auto flex sm:flex-nowrap flex-wrap">
                 <div className="lg:w-2/3 md:w-1/2 bg-gray-900 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
-                    {/* Map and Address Code */}
+                    <iframe
+                        width="100%"
+                        height="100%"
+                        title="map"
+                        className="absolute inset-0"
+                        frameBorder={0}
+                        marginHeight={0}
+                        marginWidth={0}
+                        style={{ filter: "opacity(0.7)" }}
+                        src="https://www.google.com/maps/embed/v1/search?q=Huntsville,+Texas&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8"
+                    />
+                    <div className="bg-gray-900 relative flex flex-wrap py-6 rounded shadow-md">
+                        <div className="lg:w-1/2 px-6">
+                            <h2 className="title-font font-semibold text-white tracking-widest text-xs">
+                                ADDRESS
+                            </h2>
+                            <p className="mt-1">
+                                97 Warren St. <br />
+                                New York, NY 10007
+                            </p>
+                        </div>
+                        <div className="lg:w-1/2 px-6 mt-4 lg:mt-0">
+                            <h2 className="title-font font-semibold text-white tracking-widest text-xs">
+                                EMAIL
+                            </h2>
+                            <a className="text-indigo-400 leading-relaxed">
+                                jonathanbuckner@me.com
+                            </a>
+                            <h2 className="title-font font-semibold text-white tracking-widest text-xs mt-4">
+                                PHONE
+                            </h2>
+                            <p className="leading-relaxed">214-240-4886</p>
+                        </div>
+                    </div>
                 </div>
                 <form
                     name="contact"
                     method="POST"
                     data-netlify="true"
                     data-netlify-honeypot="bot-field"
+                    onSubmit={handleSubmit}
                     className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
 
                     {/* Hidden input to pass form name */}
